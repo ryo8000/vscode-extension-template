9171ca5da9a3d277329d8b53883fb9852e273655
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-06-07 15:02:56 +0900

    Update extension.ts

--- sample-extension/src/extension.ts
+++ sample-extension/src/extension.ts
@@ -5,6 +5,7 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   // eslint-disable-next-line no-console
@@ -12,6 +13,7 @@ export function activate(context: vscode.ExtensionContext): void {
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand('sample-extension.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
