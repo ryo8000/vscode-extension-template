63cbb40cac6160b60a8d0bacf3ce6e3e3b70ac54
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2026-08-11 13:28:05 +0900

    Convert the values in each field to placeholders

--- src/extension.ts
+++ src/extension.ts
@@ -9,15 +9,15 @@ export function activate(context: vscode.ExtensionContext): void {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   // eslint-disable-next-line no-console
-  console.log('Congratulations, your extension "sample-extension" is now active!');
+  console.log('Congratulations, your extension "my-extension" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('sample-extension.helloWorld', () => {
+  const disposable = vscode.commands.registerCommand('my-extension.helloWorld', () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from sample-extension!');
+    vscode.window.showInformationMessage('Hello World from my-extension!');
   });
 
   context.subscriptions.push(disposable);
